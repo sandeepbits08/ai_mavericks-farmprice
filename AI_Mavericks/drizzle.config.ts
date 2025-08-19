c00059f6513fd0216487b4b68c3f4e26e448017e
sandeepbits08 <151496561+sandeepbits08@users.noreply.github.com>
2025-08-19 20:36:25 +0530

    Update drizzle.config.ts

--- AI_Mavericks/drizzle.config.ts
+++ AI_Mavericks/drizzle.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
+console.log("Available environment variables:", process.env);
+
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
